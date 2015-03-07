ea2ba5f3ba93ca49842d66d7b6e08ce95df1084a
Austin Wei <awei01@gmail.com>
2015-03-07 13:20:05 -0500

    adding an initial removal of ./.split in case it's there

--- build/split.sh
+++ build/split.sh
@@ -1,3 +1,4 @@
+rm -r -f .subsplit/
 git subsplit init git@github.com:awei01/pressor.git
 git subsplit publish --heads="master" mu-plugin:git@github.com:pressor/mu-plugin.git
 git subsplit publish --heads="master" framework:git@github.com:pressor/framework.git
